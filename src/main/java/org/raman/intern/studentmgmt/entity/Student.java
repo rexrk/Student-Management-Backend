064c233470bc15879cb7bdb9e63666b35314c028
Raman Kumar <kraman938@gmail.com>
2024-09-01 20:40:00 +0530

    validations fixed

--- src/main/java/org/raman/intern/studentmgmt/entity/Student.java
+++ src/main/java/org/raman/intern/studentmgmt/entity/Student.java
@@ -1,8 +1,5 @@
 package org.raman.intern.studentmgmt.entity;
-import jakarta.validation.constraints.Min;
-import jakarta.validation.constraints.NotEmpty;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Size;
+import jakarta.validation.constraints.*;
 import lombok.*;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
@@ -17,17 +14,18 @@ public class Student {
     private String id;
 
     @NotEmpty(message = "Name is required")
-    @Size(min = 2, max = 100, message = "Name must be between 2 and 100 characters")
+    @Size(min = 5, max = 50, message = "mame should be between 5 and 50 characters")
     private String name;
 
     @NotNull(message = "Contact is required")
-    @Min(value = 1000000000L, message = "Contact must be a valid 10-digit number")
+    @Digits(integer = 10, fraction = 0, message = "contact must be 10 digits number")
     private Long contactDetails;
 
     @NotEmpty(message = "Address is required")
     private String address;
 
     @NotNull(message = "Pin code is required")
-    @Min(value = 100000, message = "Pin code must be a 6-digit number")
+    @Digits(integer = 6, fraction = 0, message = "pin code must be 6 digits")
     private Integer pinCode;
+
 }
